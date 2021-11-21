48de0c9f16ff8f1c8fdb6d51e3d626dda6be24a1
punXt3r <punterparth@gmail.com>
2021-11-21 22:32:27 +0530

    Initial Commit

---
+++ src/components/Success.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+
+export class Success extends Component {
+  continue = e => {
+    e.preventDefault();
+    // PROCESS FORM //
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Success" />
+            <h1>You have already voted</h1>
+            <p>Please check your UIDAI registered mobile number for further updates</p>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Success;
